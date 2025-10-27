32f625e2b8df01b7caa7adaba7a49ef8c25b680a
Emily Middleton <emiddleton@smith.edu>
2025-10-27 07:50:55 -0400

    adding functionality for input cleaning, allows punctuation in palindromes

--- palindrome.js
+++ palindrome.js
@@ -1,17 +1,21 @@
 
-
 /**
- * Function to determine if a phrase, word, or input (provided as a string) is a palindrom
- * @param {string} input the given input to be checked (case sensitive)
- * @returns {boolean} true or false the input is a palindrom
+ * Function to determine if a phrase, word, or input (provided as a string) is a palindrome
+ * @param {string} input the given input to be checked 
+ * @returns {boolean} true or false the input is a palindrome
  */
 const isPalindrome = (input) => {
+  // cleans the input by converting to lower case and removing whitespace
+  const word = input.toLowerCase();
+  // cleaning punctuation allows 'a man a plan a canal: Panama' example to pass
+  const cleanWord = word.replaceAll(/[^a-zA-Z0-9]/g, '');
+
   let left = 0;
-  let right = input.length - 1;
+  let right = cleanWord.length - 1;
 
   // two pointer technique scans the input from either end for efficiency
   while (left < right) {
-    if (input.charAt(left) !== input.charAt(right)) {
+    if (cleanWord.charAt(left) !== cleanWord.charAt(right)) {
       return false;
     }
     left = left + 1;
@@ -21,9 +25,21 @@ const isPalindrome = (input) => {
   return true;
 }
 
-console.log(palindrome('')); // correctly returns true, the empty string is a palindrome
-console.log(palindrome('m')); // single character is a palindrome
-console.log(palindrome('mooooooooooooom'))
-console.log(palindrome('ma')); // not a palindrome
 
-console.log(palindrome('Mom'));
+// testing a complicated phrase with whitespace, capitals, and punctuation
+console.log(isPalindrome('a man a plan a canal: Panama'));
+
+// empty string
+console.log(isPalindrome(''));
+
+// single character
+console.log(isPalindrome('m'));
+
+// typical palindrome
+console.log(isPalindrome('mom'));
+
+// numerical palindrome provided as a string
+console.log(isPalindrome('404'));
+
+// test failure, nonpalindrome
+console.log(isPalindrome('not a palindrome'));
